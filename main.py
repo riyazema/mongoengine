bf454b32d01c7594ae9b91d340bc8c7c81f55c89
riyazema <riyaz.easemyai@gmail.com>
2025-03-12 17:11:49 +0530

    added jwt

--- main.py
+++ main.py
@@ -1,12 +1,134 @@
-from fastapi import FastAPI, HTTPException, UploadFile, Query
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, UploadFile, Query, Depends, Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel, EmailStr
 from mongoengine import *
 from bson.objectid import ObjectId
 import pandas as pd
 import os
+import jwt
+import time
 
 
 connect(host='mongodb://admin:%40ccessDenied321@192.168.1.42:27017/riyazdb?authSource=admin')
+
+app = FastAPI()
+
+
+
+# emp login and signup and jwt token------------------------------------------------------------
+
+JWT_SECRET = "please_please_update_me_pleasekhgkj39586yh34uth58gh9uewrhg8j"
+JWT_ALGORITHM = "HS256"
+
+
+def token_response(token: str):
+    return {
+        "access_token": token
+    }
+
+
+def sign_jwt(user_id: str):
+    payload = {
+        "user_id": user_id,
+        "expires": time.time() + 200
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token_response(token)
+
+
+def decode_jwt(token: str):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except:
+        return {}
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str):
+        isTokenValid: bool = False
+
+        try:
+            payload = decode_jwt(jwtoken)
+        except:
+            payload = None
+        if payload:
+            isTokenValid = True
+
+        return isTokenValid
+    
+
+
+
+class EmpSchema(BaseModel):
+    fullname: str
+    email: EmailStr
+    password: str
+
+
+class EmpLoginSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Emp(Document):
+    fullname = StringField(required=True)
+    email = EmailField(required=True)
+    password = StringField(required=True)
+
+
+@app.post("/emp/signup", tags=["emp"])
+async def create_emp(emp: EmpSchema):
+    try:
+        emp = Emp(**dict(emp))
+        emp.save()
+        return sign_jwt(emp.email)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+def check_emp(data: EmpLoginSchema):
+    emp = Emp.objects(email=data.email).first()
+    if emp:
+        return emp.password == data.password
+    return False
+
+
+@app.post("/emp/login", tags=["emp"])
+async def emp_login(emp: EmpLoginSchema):
+    try:
+        if check_emp(emp):
+            return sign_jwt(emp.email)
+        return {
+            "error": "Wrong login details!"
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+
+
+
+
+
+# user crud operations------------------------------------------------------------
+
 
 class User(Document):
     srno = IntField(required=True)
@@ -26,8 +148,6 @@ class UserModel(BaseModel):
     age: int
     code: int
 
-
-app = FastAPI()
 
 def user_to_dict(user):
     return {
@@ -161,7 +281,7 @@ async def get_users(srno: int = None, first_name: str = None, last_name: str = N
 
 
 # export data to excel
-@app.get("/users/export_excel/")
+@app.get("/users/export_excel/", dependencies=[Depends(JWTBearer())], tags=["posts"])
 async def export_users_to_excel():
     try:
         users = User.objects()
@@ -191,4 +311,8 @@ async def export_users_to_excel():
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error {e}")
     
+
+
+
+
 
